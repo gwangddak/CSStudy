6f848acef612f16445847fb5804793ad42ba2472
yhg0925 <yhg0925@gmail.com>
2022-07-21 17:24:18 +0900

    2022/07/21 Ch03 수업예제

---
+++ Ch03/3_For문.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/07/13 
+ * 이름 : 조광현
+ * 내용 : 반복문 for 실습하기 교재 p157
+ * 
+ * for 초기식 조건식 증감식 순서
+ */
+namespace Ch03
+{
+    internal class _3_For문
+    {
+        static void Main3(string[] args)
+        {
+            //for 문
+            for (int i = 1; i <=5 ; i++)
+            {
+                Console.WriteLine("{0}회 반복 ...",i);
+
+            }
+
+            //1부터 10까지 합
+            int sum = 0;
+            for (int k = 1; k <= 10; k++)
+            {
+                sum += k; // 복대연
+
+            }
+            Console.WriteLine("1부터 10까지 합 : " + sum);
+
+            //1부터 10까지 짝수합
+            int tot = 0;
+            for (int k = 1; k <= 10; k++)
+            {
+                if (k % 2 == 0)
+                {
+                    tot += k;
+                }
+            }
+            Console.WriteLine(tot);
+            //중첩 for문
+            for (int a = 1; a <= 3; a++)
+            {
+                Console.WriteLine("a : "+a);
+                for (int b = 1; b <= 4; b++)
+                {
+                    Console.WriteLine("b : " + b);
+
+                }
+            }
+            //구구단
+            for (int x = 2; x < 9; x++)
+            {
+                Console.WriteLine("{0}단 ",x);
+                for (int y = 1; y < 10; y++)
+                {
+                    int z = x * y;
+                    Console.WriteLine("{0} x {1} = {2} : ", x,y,z);
+                }
+            }
+            //별삼각형
+            for (int start = 1; start <= 10; start++)
+            {
+                for (int end = 1; end <= 10-start; end++)
+                {
+                    Console.Write("  ");
+                }
+                for (int Last = 1; Last <= start *2-1; Last++)
+                {
+                    Console.Write("★");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ch03/4_While문.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 
+ * 날짜 : 2022/07/13
+ * 이름 : 조광현     
+ * 내용 : 반복문 while 실습하기 교재 p162
+ */
+namespace Ch03
+{
+    internal class _4_While문
+    {
+        static void Main4(string[] args)
+        {
+            //1부터 10까지 합
+            int sum = 0;
+            int k = 1;
+
+            while (k <= 10)
+            {
+                sum += k;
+                k++; // k값 증가 
+            }
+            Console.WriteLine("1부터 10까지의 합 : "+sum);
+
+            //do ~ while 최초 한번 반복을 수행하는 반복문 
+            int total = 0;
+            int total2 = 0;
+            int i = 1;
+
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    total += i;
+              
+
+                }
+                else if(i % 2 == 1)
+                {
+                    total2 += i;
+                }
+                i++;
+
+            } while (i <= 10);
+
+            Console.WriteLine("1부터 10까지의 짝수 합 : " + total);
+            Console.WriteLine("1부터 10까지의 짝수 합 : " + total2);
+
+            //break
+
+            int num = 1;
+
+            while (true)
+            {
+                if (num % 5 == 0 && num % 7 == 0)
+                {
+                    break; // 반복문 탈출 
+                }
+                num++;
+
+            }
+
+            Console.WriteLine("5와 7의 최소 공배수 : "+num);
+            //continue
+
+            int tot = 0;
+            int tot2 = 0;
+            int n = 0;
+
+            while (n<=10)
+            {
+                n++;
+                if (n % 2 == 1)
+                {   
+                    //홀수이면 
+                    continue; // 반복문의 처음으로 이동 
+                }
+                //짝수이면
+                tot += n;
+            }
+            Console.WriteLine("1부터 10까지 짝수합 : " +tot);
+
+
+        }
+    }
+}
